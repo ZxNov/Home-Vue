05f5d8ee07eda5b1c5785ec3b52dc6ebf467ed05
ZX <novzen@outlook.com>
2025-12-26 12:14:57 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="background"></div>
     <Home />
   <footer>
-    <span>© 2227 Made By <a href="/" target="_blank">{ZXME}</a></span>
+    <span>© 2227 Made By <a href="/" target="_blank">{ ZXME }</a></span>
     <a v-if="icpNumber" href="https://beian.miit.gov.cn/" target="_blank">{{ icpNumber }}</a>
     <a v-if="policenumber" :href="`https://beian.mps.gov.cn/#/query/webSearch?police=${policenumber}`" target="_blank" class="police_link">
       <span class="police_img"></span> {{ policenumber }}
